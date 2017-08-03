0ef619a40d7615824bdaf38c4844da7a8bd0a721
Lukas A. Mueller <hello@lukasam.com>
2017-08-03 16:15:47 +0200

    Start a typescript file

---
+++ 12-uppercaser.js
@@ -0,0 +1,5 @@
+let server = require('http').createServer((req, res) => {
+
+});
+
+server.listen(process.argv[2]);
---
+++ 13-http-json-server.ts
@@ -0,0 +1,16 @@
+import * as http from 'http';
+import * as url from 'url';
+
+http.createServer((req, res) => {
+
+  // switch (req.url) {
+  //
+  //   const parsedUrl = url.parse(req.url, true);
+  //
+  //   case '/api/parsetime':
+  //
+  //     break;
+  //   case '/api/unixtime':
+  //     break;
+  // }
+});
